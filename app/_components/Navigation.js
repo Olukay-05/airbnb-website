a6903401474eb86555126d8321db1b88707cbd2b
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2024-10-23 21:39:35 +0100

    feat: Implement user session

--- app/_components/Navigation.js
+++ app/_components/Navigation.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
+import { auth } from "../_lib/auth";
 
-export default function Navigation() {
+export default async function Navigation() {
+  const session = await auth();
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
@@ -21,12 +23,29 @@ export default function Navigation() {
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center justify-center gap-2"
+            >
+              <img
+                src={session.user.image}
+                alt={session.user.name}
+                className="rounded-full h-8"
+                referrerPolicy="no-referrer"
+              />
+              <span className="grid grid-cols-1 gap-2">
+                Hi {session.user.name}. Guest area
+              </span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
