8d5ca99dd9ff097022c8567d2a7057496e94fe4c
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2024-10-25 21:29:41 +0100

    feat: implement user sign in, sign out and created new guest

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { getGuest, createGuest } from "./data-service";
 
 const authConfig = {
   providers: [
@@ -13,30 +14,33 @@ const authConfig = {
     authorized({ auth, request }) {
       return !!auth?.user;
     },
-    // async signIn({ user, account, profile }) {
-    //   try {
-    //     const existingGuest = await getGuest(user.email);
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
 
-    //     if (!existingGuest)
-    //       await createGuest({ email: user.email, fullName: user.name });
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
 
-    //     return true;
-    //   } catch {
-    //     return false;
-    //   }
-    // },
+        return true;
+      } catch {
+        return false;
+      }
+    },
+
     // async session({ session, user }) {
     //   const guest = await getGuest(session.user.email);
     //   session.user.guestId = guest.id;
     //   return session;
     // },
   },
-  //   pages: {
-  //     signIn: "/login",
-  //   },
+  pages: {
+    signIn: "/login",
+  },
 };
 
 export const {
   auth,
+  signIn,
+  signOut,
   handlers: { GET, POST },
 } = NextAuth(authConfig);
