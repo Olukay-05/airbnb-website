e9f66b5a0b136178f52de9358d98d24130c2caea
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2024-10-26 14:14:05 +0100

    feat: Deleting a reservation

--- app/account/reservations/page.js
+++ app/account/reservations/page.js
@@ -1,17 +1,19 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import { getBookings } from "@/app/_lib/data-service";
+import { auth } from "@/app/_lib/auth";
 
 export const metadata = {
   title: "Reservations",
 };
 
-export default function Page() {
-  // CHANGE
-  const bookings = [];
+export default async function Page() {
+  const session = await auth();
+  const bookings = await getBookings(session.user.guestId);
 
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Your reservations
+        Your reservation history.
       </h2>
 
       {bookings.length === 0 ? (
