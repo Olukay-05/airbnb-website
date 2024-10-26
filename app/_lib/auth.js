26da71fa4eb0defcc070b487c4185d67b9ce2df8
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2024-10-26 13:09:07 +0100

    feat: Updating user profile using server actions

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -27,11 +27,11 @@ const authConfig = {
       }
     },
 
-    // async session({ session, user }) {
-    //   const guest = await getGuest(session.user.email);
-    //   session.user.guestId = guest.id;
-    //   return session;
-    // },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
+    },
   },
   pages: {
     signIn: "/login",
